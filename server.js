a209bfb6bb896645334355b685da068766f41d19
Eric Polanco <ericpolancot@gmail.com>
2023-05-15 20:58:31 -0400

    Header template using title variable

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 // Require modules
-const express = require('express');
+const express = require('express')
+const path = require('path')
 
 // Create the Express app
-const app = express();
+const app = express()
 
 // Configure the app (app.set)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 // Mount middleware (app.use)
 
 
 // Mount routes
 app.get('/', function (req, res) {
-  res.send('<h1>Hello Express</h1>');
+  res.send('<h1>Hello Express</h1>')
 });
 
-app.get('/home', function (req, res) {
-  res.send('<h1>Home Page</h1>');
+app.get('/home', function (req, res, next) {
+  res.render('home')
 });
 
 // Tell the app to listen on port 3000
 app.listen(3000, function () {
-  console.log('Listening on port 3000');
-});
+  console.log('Listening on port 3000')
+})
